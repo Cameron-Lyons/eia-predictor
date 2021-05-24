4f1edc3878978aa35f821714c24bbfb48ac2eb86
Cameron Lyons <cameron.lyons2@gmail.com>
2021-05-23 20:53:59 -0500

    added solar plots

--- scripts/plots.py
+++ scripts/plots.py
@@ -2,10 +2,10 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-solar = pd.read_csv("..\data\final_solar.csv")
+solar = pd.read_csv("../data/final_solar.csv")
 solar["LST_DATE"] = pd.to_datetime(solar["LST_DATE"])
 
-wind = pd.read_csv("..\data\final_wind.csv")
+wind = pd.read_csv("../data/final_wind.csv")
 wind["DATE"] = pd.to_datetime(wind["DATE"])
 wind = wind[wind["generation"] >= 0]
 
@@ -15,14 +15,14 @@ avg_solar["30day_rolling_avg"] = avg_solar.generation.rolling(30).mean()
 avg_wind = wind.groupby("DATE").mean()
 avg_wind["30day_rolling_avg"] = avg_wind.generation.rolling(30).mean()
 
-sns.lineplot(x='DATE',
+sns.lineplot(x='LST_DATE',
              y='generation',
-             data=avg_wind,
-             label='Wind Generation')
+             data=avg_solar,
+             label='Solar Generation')
 
-sns.lineplot(x='DATE',
+sns.lineplot(x='LST_DATE',
              y='30day_rolling_avg',
-             data=avg_wind,
+             data=avg_solar,
              label='30 Day Rolling Avg')
 plt.show()
 
